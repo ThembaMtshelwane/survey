08c6bf8b52790c6cb37a865bae8e16746c213a31
Themba Mtshelwane <Thembamm3@gmail.com>
2024-04-30 16:59:16 +0200

    Remove boarders and checked responsiveness

--- app/components/SurveyComponents/SurveyForm/SurveyForm.tsx
+++ app/components/SurveyComponents/SurveyForm/SurveyForm.tsx
@@ -86,72 +86,74 @@ const SurveyForm = (props: Props) => {
           Please rate your level of agreement on a scale from 1 to 5, with 1
           being "strongly agree" and 5 being "strongly disagree".
         </p>
-        <table className={styles.table}>
-          <thead className={styles.tableHead}>
-            <tr className={styles.tableRows}>
-              <th className={styles.tableHeadings}> </th>
-              <th className={styles.tableHeadings}>Strongly Agree</th>
-              <th className={styles.tableHeadings}>Agree</th>
-              <th className={styles.tableHeadings}>Neutral</th>
-              <th className={styles.tableHeadings}>Disagree</th>
-              <th className={styles.tableHeadings}>Strongly Disagree</th>
-            </tr>
-          </thead>
-          <tbody className={styles.tableBody}>
-            {activities.map((item) => (
-              <tr className={styles.tableRows} key={item.id}>
-                <td className={styles.tableDataPointQuestion}>
-                  {item.activity}
-                </td>
-                <td className={styles.tableDataPointInput}>
-                  <input
-                    type="radio"
-                    value={1}
-                    className={styles.radioInputs}
-                    name={item.id}
-                    required
-                  />
-                </td>
-                <td className={styles.tableDataPointInput}>
-                  <input
-                    type="radio"
-                    value={2}
-                    className={styles.radioInputs}
-                    name={item.id}
-                    required
-                  />
-                </td>
-                <td className={styles.tableDataPointInput}>
-                  <input
-                    type="radio"
-                    value={3}
-                    className={styles.radioInputs}
-                    name={item.id}
-                    required
-                  />
-                </td>
-                <td className={styles.tableDataPointInput}>
-                  <input
-                    type="radio"
-                    value={4}
-                    className={styles.radioInputs}
-                    name={item.id}
-                    required
-                  />
-                </td>
-                <td className={styles.tableDataPointInput}>
-                  <input
-                    type="radio"
-                    value={5}
-                    className={styles.radioInputs}
-                    name={item.id}
-                    required
-                  />
-                </td>
+        <section className={styles.tableSection}>
+          <table className={styles.table}>
+            <thead className={styles.tableHead}>
+              <tr className={styles.tableRows}>
+                <th className={styles.tableHeadings}> </th>
+                <th className={styles.tableHeadings}>Strongly Agree</th>
+                <th className={styles.tableHeadings}>Agree</th>
+                <th className={styles.tableHeadings}>Neutral</th>
+                <th className={styles.tableHeadings}>Disagree</th>
+                <th className={styles.tableHeadings}>Strongly Disagree</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className={styles.tableBody}>
+              {activities.map((item) => (
+                <tr className={styles.tableRows} key={item.id}>
+                  <td className={styles.tableDataPointQuestion}>
+                    {item.activity}
+                  </td>
+                  <td className={styles.tableDataPointInput}>
+                    <input
+                      type="radio"
+                      value={1}
+                      className={styles.radioInputs}
+                      name={item.id}
+                      required
+                    />
+                  </td>
+                  <td className={styles.tableDataPointInput}>
+                    <input
+                      type="radio"
+                      value={2}
+                      className={styles.radioInputs}
+                      name={item.id}
+                      required
+                    />
+                  </td>
+                  <td className={styles.tableDataPointInput}>
+                    <input
+                      type="radio"
+                      value={3}
+                      className={styles.radioInputs}
+                      name={item.id}
+                      required
+                    />
+                  </td>
+                  <td className={styles.tableDataPointInput}>
+                    <input
+                      type="radio"
+                      value={4}
+                      className={styles.radioInputs}
+                      name={item.id}
+                      required
+                    />
+                  </td>
+                  <td className={styles.tableDataPointInput}>
+                    <input
+                      type="radio"
+                      value={5}
+                      className={styles.radioInputs}
+                      name={item.id}
+                      required
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
       </section>
       <section className={styles.buttonContainer}>
         <input type="submit" className={styles.button} />
