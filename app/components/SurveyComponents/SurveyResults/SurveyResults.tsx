ebdb157de0febaf65d1cccd3b4026c7e2e3850e5
Themba Mtshelwane <Thembamm3@gmail.com>
2024-05-02 18:43:35 +0200

    survey results ui

--- app/components/SurveyComponents/SurveyResults/SurveyResults.tsx
+++ app/components/SurveyComponents/SurveyResults/SurveyResults.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import styles from './surveyresults.module.css'
+
 import { getData, getStoredData } from '@/app/actions/actions'
 import { AgeStats, AllUsersInfo, StatData } from '@/app/lib/definitions'
 import {
@@ -55,27 +57,56 @@ const SurveyResults = (props: Props) => {
   return (
     <div>
       {ageStatistics && foodStatistics && activityStatistics ? (
-        <div>
-          <div>
-            <div>Oldest: {ageStatistics.oldest}</div>
-            <div>Youngest: {ageStatistics.youngest}</div>
-            <div>Average: {ageStatistics.average}</div>
-          </div>
-          <div>
-            {foodStatistics.map((food) => (
-              <div key={food.label}>
-                {food.label}: {food.value}%
+        <section className={styles.resultsContainer}>
+          <section className={styles.heading}>Survey Results</section>
+          <section className={styles.data}>
+            <section className={styles.personalData}>
+              <section className={styles.dataPoint}>
+                <div className={styles.label}>Total number of survey:</div>
+                <div className={styles.value}>{allUsersData?.numOfSurveys}</div>
+              </section>
+              <section className={styles.dataPoint}>
+                <div className={styles.label}>Average:</div>
+                <div className={styles.value}> {ageStatistics.average}</div>
+              </section>
+
+              <section className={styles.dataPoint}>
+                <div className={styles.label}>
+                  Oldest person who participated in survey:
+                </div>
+                <div className={styles.value}>{ageStatistics.oldest}</div>
+              </section>
+              <section className={styles.dataPoint}>
+                <div className={styles.label}>
+                  Youngest person who participated in survey:
+                </div>
+                <div className={styles.value}>{ageStatistics.youngest}</div>
+              </section>
+            </section>
+
+            <section className={styles.foodData}>
+              {foodStatistics.map((food) => (
+                <div key={food.label} className={styles.dataPoint}>
+                  <div className={styles.label}>
+                    Percentage of people who like {food.label} :
+                  </div>
+                  <div className={styles.value}>{food.value}%</div>
+                </div>
+              ))}
+            </section>
+
+            <section className={styles.activityData}>
+              <div>
+                {activityStatistics.map((activity) => (
+                  <div key={activity.label} className={styles.dataPoint}>
+                    <div> People who like to {activity.label}:</div>
+                    <div className={styles.value}>{activity.value}%</div>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-          <div>
-            {activityStatistics.map((activity) => (
-              <div key={activity.label}>
-                {activity.label}: {activity.value}
-              </div>
-            ))}
-          </div>
-        </div>
+            </section>
+          </section>
+        </section>
       ) : allUsersData ? (
         <div>Loading...</div>
       ) : (
