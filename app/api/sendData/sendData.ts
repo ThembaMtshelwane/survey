4f861c80438f309431da46968a1df9e4c311c3ef
Themba Mtshelwane <Thembamm3@gmail.com>
2024-05-02 17:04:54 +0200

    api routes update

---
+++ app/api/sendData/sendData.ts
@@ -0,0 +1,37 @@
+import { db } from "@/app/firebase/firebaseConfig"
+import { activities } from "@/app/lib/data"
+import { ActivityData } from "@/app/lib/definitions"
+import { addDoc, collection, doc, setDoc } from "firebase/firestore"
+
+export const POST = async (req: Request) => {
+  const data = await req.formData()
+  const activityRatings: ActivityData[] = []
+
+  activities.map((item) => {
+    activityRatings.push({ nameOfActivity: item.id, count: data.get(item.id) })
+  })
+
+  const userData = {
+    fullNames: data.get('fullNames'),
+    email: data.get('email'),
+    birthDate: data.get('birthDate'),
+    selectedFoods: Array.from(data.getAll('foodItem')),
+    activityRatings,
+  }
+  try {
+    const docRef = await addDoc(collection(db, 'users'), userData)
+
+    const foodDocRef = doc(db, 'allFoodPreferences', docRef.id)
+    await setDoc(foodDocRef, { selectedFoods: userData.selectedFoods })
+
+    const activityDocRef = doc(db, 'allActivityRatings', docRef.id)
+    await setDoc(activityDocRef, { activityRatings: userData.activityRatings })
+
+    return Response.json({
+      message: 'Data sent successfully',
+      status: 'success',
+    })
+  } catch (e) {
+    return Response.json({ message: `Error : ${e}`, status: 'fail' })
+  }
+}
