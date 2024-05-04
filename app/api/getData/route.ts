cfc8116733b253b56df360b57c4a8d511488fed6
Themba Mtshelwane <Thembamm3@gmail.com>
2024-05-04 23:07:47 +0200

    adding cors and cache

--- app/api/getData/route.ts
+++ app/api/getData/route.ts
@@ -1,9 +1,30 @@
 import { db } from '@/app/firebase/firebaseConfig'
 import { ActivityData } from '@/app/lib/definitions'
 import { getDocs, collection } from 'firebase/firestore'
+import { NextApiResponse } from 'next'
 import { NextResponse } from 'next/server'
+import Cors from 'cors'
 
-export const GET = async (req: Request) => {
+const cors = Cors({
+  methods: ['GET'], // Allow only GET requests
+  origin: process.env.NEXT_PUBLIC_CLIENT_URL, // Specify the origin(s) that are allowed to make requests
+})
+
+function runMiddleware(req: any, res: NextApiResponse, fn: any) {
+  return new Promise<void>((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+      return resolve(result)
+    })
+  })
+}
+export const GET = async (req: Request, res: NextApiResponse) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+  res.setHeader('Pragma', 'no-cache')
+  res.setHeader('Expires', '0')
+  await runMiddleware(req, res, cors)
   try {
     let usersAges: number[] = []
     let usersFoodPreferences: string[] = []
@@ -28,17 +49,18 @@ export const GET = async (req: Request) => {
       usersActivityRatings.push(...doc.data().activityRatings)
     })
 
-    console.log('All users fetched successfully')
-    return NextResponse.json({
+    const data = {
       emails,
       usersAges: usersAges,
       numOfSurveys,
       usersFoodPreferences: usersFoodPreferences,
       usersActivityRatings: usersActivityRatings,
-    })
+    }
+
+    console.log('All users fetched successfully', data)
+    return NextResponse.json(data)
   } catch (error) {
     console.error('Error fetching users: ', error)
     throw error
   }
 }
-
